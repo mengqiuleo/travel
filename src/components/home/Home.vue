eaa425d4bb9ae157a2cf77f2070cfb41ae2e83b3
潘静怡 <1003346758@qq.com>
2022-02-15 05:36:07 +0800

    projet init

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,15 +1,17 @@
 <template>
-  <div id="app">
-    hello 
+  <div>
+    hello world
   </div>
 </template>
 
 <script>
+
 export default {
-  name: 'App'
+  name: 'Home'
 }
+
 </script>
 
-<style>
+<style scoped>
 
 </style>
